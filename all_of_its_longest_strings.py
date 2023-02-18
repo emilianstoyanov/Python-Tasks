965fe904bc24a4f940908e8e1bf6a22d8e7e1102
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-02-18 17:40:07 +0200

    update

--- all_of_its_longest_strings.py
+++ all_of_its_longest_strings.py
@@ -37,15 +37,15 @@ print(solution(["aba", "aa", "ad", "vcd", "aba"]))
 # Input/Output
 
     # [execution time limit] 4 seconds (py3)
-    #
+
     # [input] array.string inputArray
-    #
+
     # A non-empty array.
-    #
+
     # Guaranteed constraints:
     # 1 ≤ inputArray.length ≤ 10,
     # 1 ≤ inputArray[i].length ≤ 10.
-    #
+
     # [output] array.string
-    #
+
     # Array of the longest strings, stored in the same order as in the inputArray.
