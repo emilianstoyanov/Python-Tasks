149fb98c09d6f035603758e4fb79d138be10d4e0
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-03-04 14:20:36 +0200

    move zeroes

---
+++ algorithms_for_coding_interview/move_zeroes.py
@@ -0,0 +1,20 @@
+# Given an array nums, write a function to move all zeroes to the end of it while maintaining the relative order of
+# the non-zero elements.
+
+
+def solution(nums):
+    for i in nums:
+        if 0 in nums:
+            nums.remove(0)
+            nums.append(0)
+    return nums
+
+
+array1 = [0, 1, 0, 3, 12]
+print(solution(array1))
+array2 = [1, 7, 0, 0, 8, 0, 10, 12, 0, 4]
+print(solution(array2))
+
+# Output:
+# [1, 3, 12, 0, 0]
+# [1, 7, 8, 10, 12, 4, 0, 0, 0, 0]
