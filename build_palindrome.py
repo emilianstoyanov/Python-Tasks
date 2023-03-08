ad48eeaf1791677ffccf1e95514967970b1b099b
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-03-08 15:56:36 +0200

    update text

--- build_palindrome.py
+++ build_palindrome.py
@@ -11,9 +11,38 @@ def solution(st):
     string_list = list(st)
     i = 0
     while string_list != string_list[::-1]:
-        string_list.insert(len(st),st[i])
+        string_list.insert(len(st), st[i])
         i += 1
     return "".join(string_list)
 
 
 print(solution('abcdc'))
+# abcdcba
+
+
+print(solution('ababab'))
+# abababa
+
+print(solution('abba'))
+# abba
+
+# Given a string, find the shortest possible string which can be achieved by adding characters
+# to the end of initial string to make it a palindrome.
+
+# Example
+
+    # For st = "abcdc", the output should be
+    # solution(st) = "abcdcba".
+
+# Input/Output
+
+    # [execution time limit] 4 seconds (py3)
+
+    # [input] string st
+
+    # A string consisting of lowercase English letters.
+
+    # Guaranteed constraints:
+    # 3 ≤ st.length ≤ 10.
+
+    # [output] string
